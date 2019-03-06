077b1037668602320d686c330c524174724b083c
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2019-03-06 11:45:25 +0100

    fixed indentation

--- src/transcriberbot/tbfilters.py
+++ src/transcriberbot/tbfilters.py
@@ -1,20 +1,20 @@
 from telegram.ext import BaseFilter
 
 class FilterIsAdmin(BaseFilter):
-	def __init__(self, bot):
-		self.bot = bot
+  def __init__(self, bot):
+    self.bot = bot
 
-	def filter(self, message):
-		if message.chat.id >= 0:
-			return True
+  def filter(self, message):
+    if message.chat.id >= 0:
+      return True
 
-		if message.chat.type == 'channel':
-			return True
+    if message.chat.type == 'channel':
+      return True
 
-		sender = message.from_user
-		admins = message.chat.get_administrators()
+    sender = message.from_user
+    admins = message.chat.get_administrators()
 
-		is_admin = list(filter(lambda cm: cm.user.id == sender.id, admins))
-		is_admin = len(is_admin) > 0
+    is_admin = list(filter(lambda cm: cm.user.id == sender.id, admins))
+    is_admin = len(is_admin) > 0
 
-		return is_admin
+    return is_admin
