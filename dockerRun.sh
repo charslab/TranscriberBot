e75801721a4b6d8e5840a500785569a868b54a9e
stefanodelbosco <stefano.delbosco2@gmail.com>
2020-07-29 20:33:43 +0200

    Added name of container in dockerRun

--- dockerRun.sh
+++ dockerRun.sh
@@ -1,6 +1,7 @@
 docker run \
    -e LC_ALL=C \
    -d --restart unless-stopped \
+   --name "transcriberbot" \
    -v "$(pwd)"/data:/data \
    -v "$(pwd)"/config:/config \
    -v "$(pwd)"/values:/values \
