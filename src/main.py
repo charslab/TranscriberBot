af2d5581e9b36918445de0d9398dc41124f7f735
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2025-02-16 22:08:55 +0100

    add sentry

--- src/main.py
+++ src/main.py
@@ -5,7 +5,8 @@ import resources
 import database
 import antiflood
 import transcriberbot.bot
-
+import sentry_sdk
+from sentry_sdk.integrations.asyncio import AsyncioIntegration
 
 def main():
     config.init('../config')
@@ -23,6 +24,24 @@ def main():
     antiflood.init()
     database.init_schema(config.get_config_prop("app")["database"])
 
+    sentry_sdk.init(
+        dsn=config.get_config_prop("sentry")["dsn"],
+        # Add data like request headers and IP for users, if applicable;
+        # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+        send_default_pii=True,
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for tracing.
+        traces_sample_rate=1.0,
+        # Set profiles_sample_rate to 1.0 to profile 100%
+        # of sampled transactions.
+        # We recommend adjusting this value in production.
+        profiles_sample_rate=1.0,
+        integrations=[
+            AsyncioIntegration(),
+        ],
+    )
+
+    sentry_sdk.profiler.start_profiler()
     transcriberbot.bot.run(config.bot_token())
 
 
