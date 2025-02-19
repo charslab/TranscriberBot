7fa93371a2b5186c00b1ac1cfa3a2fd08faac440
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2025-02-19 21:02:30 +0100

    update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,15 @@
+#!/bin/sh
+
+docker pull ghcr.io/charslab/transcriberbot:ptb-async
 docker run \
    -e LC_ALL=C \
    -d --restart unless-stopped \
-   --name "transcriberbot" \
+   --name "transcriberbot-async" \
    -v "$(pwd)"/data:/data \
    -v "$(pwd)"/config:/config \
    -v "$(pwd)"/values:/values \
-   transcriberbot
+   -v "$(pwd)"/media:/media \
+   --cpus=4.0 \
+   --memory=3000m \
+   -u "$(id -u):1337" \
+   ghcr.io/charslab/transcriberbot:ptb-async
