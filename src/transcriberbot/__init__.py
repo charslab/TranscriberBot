a674c477721b4d7dfe7f611dc962d68e5c059de0
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2025-02-20 23:07:44 +0100

    remove unused files

--- src/transcriberbot/__init__.py
+++
@@ -1,4 +0,0 @@
-from transcriberbot.bot import TranscriberBot
-from transcriberbot.bot import message
-from transcriberbot.bot import init
-from transcriberbot import handlers_commands, handlers_messages, handlers_query
